90a819fe0f8bd737e668418ed2f9d4ab9fb0010d
dirtysquidstack <130407203+dirtysquidstack@users.noreply.github.com>
2023-10-24 16:24:03 -0700

    added rounds and scoring

--- prc.js
+++ prc.js
@@ -2,16 +2,84 @@ const prompt = require("prompt-sync")();
 
 function getComputerChoice()
 {
-    const prc = ["Rock", "Paper", "Scissors"];
+    const prc = ["rock", "paper", "scissors"];
     const random= Math.floor(Math.random() * prc.length);
     return prc[random];
 }
 
-//check computer choice
-console.log(getComputerChoice());
+let computerscore = 0
+let playerscore = 0
 
-let playerPrompt = prompt ("Paper, Rock, or Scissors? ");
-let playerSelection = playerPrompt.toLowerCase();
+function oneRound(playerSelection, computerSelection)
+{
+    if (playerSelection === computerSelection)
+    { 
+        return "it's a tie!"
+    }
 
-//check if player selection works
-console.log(playerSelection);
+    else if (playerSelection == "rock" && computerSelection == "scissors")
+    { 
+        playerscore++;
+        return "hell yeah brother! rock beats scissors!"
+    }
+
+    else if (playerSelection == "rock" && computerSelection == "paper")
+    {
+        computerscore++;
+        return "yo you suck, paper beats rock!"
+    }
+
+    else if (playerSelection == "paper" && computerSelection == "rock")
+    {
+        playerscore++;
+        return "ayo! paper beats rock!"
+    }
+
+    else if (playerSelection == "paper" && computerSelection == "scissors")
+    {   
+        computerscore++
+        return "Boooo scissors beats paper"
+    }
+
+    else if (playerSelection == "scissors" && computerSelection == "rock")
+    { 
+        computerscore++;
+        return "oof. rock beats scissors."
+    }
+
+    else if (playerSelection == "scissors" && computerSelection == "paper")
+    { 
+        playerscore++;
+        return "hell yeah, scissors slashes paper"
+    }
+
+    else
+    {
+        return "u wrong";
+    }
+}
+
+function fiveRounds()
+{
+    for (i = 0; i < 5; i++)
+    {
+        let playerPrompt = prompt("Paper, Rock, or Scissors? ");
+        let playerSelection = playerPrompt.toLowerCase();
+        let computerSelection = getComputerChoice();
+        console.log(computerSelection);
+        console.log(oneRound(playerSelection, computerSelection, computerscore, playerscore));
+        console.log(playerscore, computerscore);
+
+        if(computerscore == 3)
+        {
+            console.log("COMPUTER WINS")
+            break;
+        }
+        if(playerscore == 3)
+        {
+            console.log("PLAYER WINS")
+            break;
+        }
+    }
+}
+fiveRounds();
